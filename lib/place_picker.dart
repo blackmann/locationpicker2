439d1410530094670a37cae056e95a7015090e09
De-Great Yartey <yarteydegreat2@gmail.com>
2020-05-25 22:07:01 +0000

    expose google maps and controller

--- lib/place_picker.dart
+++ lib/place_picker.dart
@@ -1,2 +1,4 @@
 export 'src/place_picker.dart';
 export 'src/entities/entities.dart' show LocationResult;
+export 'package:google_maps_flutter/google_maps_flutter.dart'
+    show GoogleMap, GoogleMapController;
